1554cafd4cb0196fa7c3f9b5bca8f225fd8611d6
Ian Ibbotson <ianibbo@gmail.com>
2021-06-02 13:40:56 +0100

    Add unique index to refdata value - ERM-1650

--- service/grails-app/migrations/setup-refdata.groovy
+++ service/grails-app/migrations/setup-refdata.groovy
@@ -65,4 +65,9 @@ databaseChangeLog = {
     }
     addNotNullConstraint (tableName: "refdata_category", columnName: "internal", defaultNullValue: false)
   }
+
+  changeSet(author: "ianibbo (manual)", id: "202106021259-001") {
+    addUniqueConstraint(columnNames: "rdv_owner,rdv_value", constraintName: "RDV_UNIQUE_VALUE", tableName: "refdata_value")
+  }
+
 }
