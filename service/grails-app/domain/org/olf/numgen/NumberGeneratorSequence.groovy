aea7c52e1fd25e81fea603edb204c2266b4b0063
Ian Ibbotson <ianibbo@gmail.com>
2022-06-15 08:13:37 +0100

    Added checksum

--- service/grails-app/domain/org/olf/numgen/NumberGeneratorSequence.groovy
+++ service/grails-app/domain/org/olf/numgen/NumberGeneratorSequence.groovy
@@ -15,26 +15,31 @@ class NumberGeneratorSequence implements MultiTenant<NumberGeneratorSequence> {
   String format
   Long nextValue
 
+  @Defaults(['None', 'Modulo10', 'Modulo11', 'Modulo16', 'Modulo43', 'Modulo47'])
+  RefdataValue checkDigitAlgo
+
   static constraints = {
-       prefix(nullable: true)
-      postfix(nullable: true)
-    nextValue(nullable: true)
-       format(nullable: true)
+            prefix(nullable: true)
+           postfix(nullable: true)
+         nextValue(nullable: true)
+            format(nullable: true)
+    checkDigitAlgo(nullable: true)
   }
 
 
   static mapping = {
-           id column: 'ngs_id', generator: 'uuid2', length:36
-      version column: 'ngs_version'
-        owner column: 'ngs_owner'
-         code column: 'ngs_code'
-       prefix column: 'ngs_prefix'
-      postfix column: 'ngs_postfix'
-    nextValue column: 'ngs_next_value'
-       format column: 'ngs_format'
+                id column: 'ngs_id', generator: 'uuid2', length:36
+           version column: 'ngs_version'
+             owner column: 'ngs_owner'
+              code column: 'ngs_code'
+            prefix column: 'ngs_prefix'
+           postfix column: 'ngs_postfix'
+         nextValue column: 'ngs_next_value'
+            format column: 'ngs_format'
+    checkDigitAlgo column: 'ngs_check_digit_algorithm'
   }
 
   public String toString() {
-    return "NumberGeneratorSequence(${owner?.code}.${code} ${prefix} ${nextValue} ${postfix} ${format})".toString();
+    return "NumberGeneratorSequence(${owner?.code}.${code} ${prefix} ${nextValue} ${postfix} ${format} ${checkDigitAlgo?.value})".toString();
   }
 }
